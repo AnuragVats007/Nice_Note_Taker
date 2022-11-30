46bd239753e4083510f403774e3875894e3ca67f
Anurag Vats <lit2020007@iiitl.ac.in>
2022-12-01 00:01:53 +0530

    Final project

---
+++ pages/dashboard/index.js
@@ -0,0 +1,60 @@
+const cardContainer = document.querySelector(".card-container");
+const token = localStorage.getItem("jwt");
+const logout = document.querySelector(".logout");
+let cardData = [];
+const createNoteButton = document.querySelector(".new-note");
+// const apiUrl = "http://localhost:8000";
+const apiUrl="https://secure-waters-82488.herokuapp.com";
+
+
+createNoteButton.addEventListener("click", () => {
+  location.href = "/pages/createNotes/createNotes.html";
+});
+
+logout.addEventListener("click", () => {
+  localStorage.removeItem("jwt");
+  location.href = "../authPage/index.html";
+});
+
+const createNotes = (array) => {
+  cardContainer.innerHTML = "";
+
+  array.forEach((cardObj) => {
+    const { heading, content } = cardObj;
+    const id = cardObj.noteId;
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.id = id;
+
+    const insideHtml = `<div class="card-header"><div class="card-heading">${heading}</div><a href="../updateNotes/updateNotes.html?noteId=${id}"><div class="edit-note"><img src="../../assests/edit-note.svg" alt="" /></div></a></div><div class="card-content">${content}</div>`;
+
+    card.innerHTML = insideHtml;
+
+    cardContainer.appendChild(card);
+  });
+};
+
+const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+  body.classList.add("visible");
+
+  if (token) {
+    fetch(`${apiUrl}/note/getallnotes`, {
+      method: "GET",
+      headers: {
+        authorization: token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        cardData = data.data;
+        createNotes(data.data);
+      })
+      .catch((err) => {
+        alert("Error in fetching data");
+        console.log(err);
+      });
+  }
+});
---
+++ pages/updateNotes/index.js
@@ -0,0 +1,60 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const noteId = urlParams.get("noteId");
+
+const updateNoteButton = document.querySelector(".create-note-button");
+const deleteNoteButton = document.querySelector(".delete-note-button");
+
+// const apiUrl = "http://localhost:8000";
+const apiUrl="https://secure-waters-82488.herokuapp.com";
+
+
+const token = localStorage.getItem("jwt");
+
+updateNoteButton.addEventListener("click", () => {
+  const content = document.querySelector(".create-note-input").value;
+  const heading = document.querySelector(".create-note-heading").value;
+
+  if (token) {
+    fetch(`${apiUrl}/note/update/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({ content, heading }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        alert("Error Creating Note!! Re-try....");
+        console.log(err);
+      });
+  }
+});
+
+deleteNoteButton.addEventListener("click", () => {
+  if (token) {
+    fetch(`${apiUrl}/note/delete/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error Deleting Note!! Re-try....");
+      });
+  }
+});
